91668b7fa46d463533749677ecaf352bd49007a1
Luca Clementi <luca.clementi@gmail.com>
2013-11-07 09:55:51 -0800

    now the module loads properly it functions

--- limits.c
+++ limits.c
@@ -25,95 +25,131 @@
 
 #include <linux/init.h>
 #include <linux/kd.h>
+#include <linux/kallsyms.h>
+#include <linux/string.h>
 #include <linux/kernel.h>
 #include <linux/tracehook.h>
 #include <linux/errno.h>
 #include <linux/sched.h>
 #include <linux/security.h>
+#include <linux/module.h>
 
 
-static int limit_file_enabled;
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Access non-exported symbols");
+MODULE_AUTHOR("Stephen Zhang");
 
 
-static int limit_file_inode_create(struct inode *dir, struct dentry *dentry, umode_t mode)
-{
 
-	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
-}
+
 
 static int limit_file_inode_link(struct dentry *old_dentry, struct inode *dir, struct dentry *new_dentry)
 {
 	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
+	return 0;
 }
 
 static int limit_file_inode_unlink(struct inode *dir, struct dentry *dentry)
 {
 	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
+	return 0;
 }
 
 static int limit_file_inode_symlink(struct inode *dir, struct dentry *dentry, const char *name)
 {
 	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
-}
-
-static int limit_file_inode_mkdir(struct inode *dir, struct dentry *dentry, umode_t mask)
-{
-	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
-}
-
-static int limit_file_inode_mknod(struct inode *dir, struct dentry *dentry, umode_t mode, dev_t dev)
-{
-	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
+	return 0;
 }
 
 static int limit_file_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
 				struct inode *new_inode, struct dentry *new_dentry)
 {
 	printk(KERN_INFO "Limit File: limit file inode.\n");
-	return 1;
-}
-
-
-static struct security_operations limit_file_ops = {
-	.name =				"limit_files",
-
-	.inode_create =			limit_file_inode_create,
-	.inode_link =			limit_file_inode_link,
-	.inode_unlink =			limit_file_inode_unlink,
-	.inode_symlink =		limit_file_inode_symlink,
-	.inode_mkdir =			limit_file_inode_mkdir,
-	.inode_mknod =			limit_file_inode_mknod,
-	.inode_rename =			limit_file_inode_rename,
-
-};
-
-static __init int limit_file_init(void)
-{
-	if (!security_module_enable(&limit_file_ops)) {
-		limit_file_enabled = 0;
-		return 0;
-	}
-
-	if (!limit_file_enabled) {
-		printk(KERN_INFO "Limit File:  Disabled at boot.\n");
-		return 0;
-	}
-
-	printk(KERN_INFO "Limit File:  Initializing.\n");
-
-
-	if (register_security(&limit_file_ops))
-		panic("Limit File: Unable to register with kernel.\n");
-
 	return 0;
 }
 
 
-security_initcall(limit_file_init);
+
+static int cap_inode_create(struct inode *inode, struct dentry *dentry,
+			    int mask)
+{
+	printk(KERN_INFO "Limit File: inode create.\n");
+	return 0;
+}
+
+
+static int cap_inode_mkdir(struct inode *inode, struct dentry *dentry,
+			   int mask)
+{
+	printk(KERN_INFO "Limit File: inode mkdir.\n");
+	return 0;
+}
+
+
+static int cap_inode_mknod(struct inode *inode, struct dentry *dentry,
+			   int mode, dev_t dev)
+{
+	printk(KERN_INFO "Limit File: inode mknod.\n");
+	return 0;
+}
+
+static struct security_operations limit_file_ops = {
+	.name =				"limit_files",
+
+	.inode_create =			cap_inode_create,
+	.inode_link =			limit_file_inode_link,
+	.inode_unlink =			limit_file_inode_unlink,
+	.inode_symlink =		limit_file_inode_symlink,
+	.inode_mkdir =			cap_inode_mkdir,
+	.inode_mknod =			cap_inode_mknod,
+	.inode_rename =			limit_file_inode_rename,
+
+};
+
+
+unsigned long sym_addr = 0;
+
+int search_function(void * data, const char *sym_name, struct module * mod, unsigned long addres){
+	char *lookup_sym_name = (char *) data;
+
+	if (strcmp(lookup_sym_name, sym_name) == 0){
+		sym_addr = addres;
+		return 1;
+	}
+	return 0;
+}
+
+
+static int __init limit_files_init(void)
+{
+	char *sym_name = "register_security";
+
+	typedef int register_security(struct security_operations *);
+	register_security *f;
+
+	kallsyms_on_each_symbol(search_function, sym_name);
+
+	if (!sym_addr)
+		panic("Limit File: Unable to get register_security address.\n");
+
+	f = (void *) sym_addr;
+
+	if (f(&limit_file_ops))
+		panic("Limit File: Unable to register with kernel.\n");
+
+	printk(KERN_INFO "[%s] %s (0x%lx)\n", __this_module.name, sym_name, sym_addr);
+
+	return 0;
+}
+
+static void __exit limit_files_exit(void)
+{
+}
+
+module_init(limit_files_init);
+module_exit(limit_files_exit);
+
+
+
 
